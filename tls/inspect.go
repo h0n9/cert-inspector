fbd93ec0f4f94659e7ad00ca5747714cca5947d8
Jaehong Ahn <monsieur.ahn@gmail.com>
2021-07-01 12:10:24 +0900

    Add very simple getConnState()

--- tls/inspect.go
+++ tls/inspect.go
@@ -1,1 +1,25 @@
 package tls
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	"github.com/h0n9/cert-inspector/types"
+)
+
+func GetConnState(host *types.Host) (*tls.ConnectionState, error) {
+	addr := fmt.Sprintf("%s:%d", host.Hostname, host.Port)
+	conn, err := tls.Dial("tcp", addr, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	err = conn.VerifyHostname(host.Hostname)
+	if err != nil {
+		return nil, err
+	}
+
+	connectionState := conn.ConnectionState()
+	return &connectionState, nil
+}
--- types/host.go
+++ types/host.go
@@ -1,1 +1,15 @@
 package types
+
+type Host struct {
+	Hostname string `json:"hostname"`
+	Port     int    `json:"port,omitempty"`
+	Issuer   string `json:"issuer,omitempty"`
+	Expiry   string `json:"expiry,omitempty"`
+}
+
+func NewHost(hostname string, port int) *Host {
+	return &Host{
+		Hostname: hostname,
+		Port:     port,
+	}
+}
