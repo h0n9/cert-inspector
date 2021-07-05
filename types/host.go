4fc8259671386d1bb510e3fce947faeb92d5b958
Jaehong Ahn <monsieur.ahn@gmail.com>
2021-07-05 09:30:23 +0900

    Update String() content

--- types/host.go
+++ types/host.go
@@ -52,7 +52,7 @@ func (h *Host) String() string {
 		str += fmt.Sprintf("\033[1;33m%s\033[0m", "[WARN]\t")
 	}
 
-	str += fmt.Sprintf("Hostname: %s\n\tPort: %d\n\tIssuer: %s\n\tExpiry: %s\n\tDays: %d",
+	str += fmt.Sprintf("Hostname: %s\n\tPort: %d\n\tIssuer: %s\n\tExpiry Date: %s\n\tDays left for expiry: %d",
 		h.Hostname, port, h.Issuer, h.ExpDate, h.ExpDays)
 	return str
 }
