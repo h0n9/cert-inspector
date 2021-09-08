087c03ba0ef8fbcd2b6269770cb964d5c1da145c
Jaehong Ahn <monsieur.ahn@gmail.com>
2021-09-08 14:51:39 +0900

    Add IPAddr property to Host
    - prettify String()

--- types/host.go
+++ types/host.go
@@ -15,6 +15,7 @@ const (
 
 type Host struct {
 	Hostname string `json:"hostname" yaml:"hostname"`
+	IPAddr   string `json:"ip_addr" yaml:"ip_addr"`
 	Port     int    `json:"port,omitempty" yaml:"port,omitempty"`
 	Issuer   string `json:"issuer,omitempty" yaml:"issuer,omitempty"`
 	ExpDate  string `json:"exp_date,omitempty" yaml:"exp_date,omitempty"`
@@ -27,6 +28,10 @@ func NewHost(hostname string, port int) *Host {
 		Hostname: hostname,
 		Port:     port,
 	}
+}
+
+func (h *Host) SetIPAddr(ipAddr string) {
+	h.IPAddr = ipAddr
 }
 
 func (h *Host) SetIssuer(issuer string) {
@@ -53,7 +58,12 @@ func (h *Host) String() string {
 		str += util.Warn("[WARN]\t")
 	}
 
-	str += fmt.Sprintf("Hostname: %s\n\tPort: %d\n\tIssuer: %s\n\tExpiry Date: %s\n\tDays left for expiry: %d",
-		h.Hostname, port, h.Issuer, h.ExpDate, h.ExpDays)
+	str += fmt.Sprintf("Hostname: %s\n"+
+		"\tIP Address: %s\n"+
+		"\tPort: %d\n"+
+		"\tIssuer: %s\n"+
+		"\tExpiry Date: %s\n"+
+		"\tDays left for expiry: %d",
+		h.Hostname, h.IPAddr, port, h.Issuer, h.ExpDate, h.ExpDays)
 	return str
 }
